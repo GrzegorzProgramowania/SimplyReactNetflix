8d32638e2568450b5cfc07d9748bf5b42bf02cd4
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2024-01-26 08:14:40 +0100

    clear all css, add header with 2 imgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
-
+//https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940
+//https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png
 function App() {
 
   return (
     <>
-      Test SimplyReactNeftlix
+      <header>
+        <img src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940" alt="NetflixLogo"/>
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png" alt="AvatarLogo"/>
+      </header>
     </>
   )
 }
